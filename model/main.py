1fcc908d799c73434226e58387f58f106c34c2c4
lawrencj <lawrencj@mail.uc.edu>
2018-03-03 18:36:10 -0500

    -added main.py

---
+++ model/main.py
@@ -0,0 +1,22 @@
+from player import Player
+from table import Table
+game_started = False
+def ask(query):
+    x = query
+
+def main():
+
+    t = Table()
+    ask("How much money did each person bring?")
+    
+    
+def game_manager(game_started, sub_round):
+    
+    if (not game_started):
+        t.intial_count = result
+        t.big_blind = t.intial_count/100
+        t.small_blind = t.big_blind/2
+        t.blind_timer = 3
+        t.id = "conversationId"
+    
+
