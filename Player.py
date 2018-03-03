3d4f61819394a37e597e2a51b0e8fe9ee8c65cfa
lawrencj <lawrencj@mail.uc.edu>
2018-03-03 15:25:52 -0500

    -Player change

--- Player.py
+++ Player.py
@@ -1,7 +1,6 @@
 class Player:
-    name = "Caden"
-     
-    def __init__(self, name, chips, inRound):
-      self.name = name
-      self.chips = chips
-      self.inRound = inRound
+    
+	def __init__(self, name, chips, inRound):
+    	self.name = name
+    	self.chips = chips
+    	self.inRound = inRound
